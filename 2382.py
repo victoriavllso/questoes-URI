a58b87e9c2635fbc206b773ac9038b6f8a67966f
Victória Rodrigues Veloso <128431650+victoriavllso@users.noreply.github.com>
2023-03-29 09:10:58 -0300

    Update 2382.py

--- 2382.py
+++ 2382.py
@@ -1,7 +1,7 @@
 l,a,p,r = int(l), int(a), int(p), int(r)
 
-dc = (l**2 + a**2 + p**2)**0.5
-de = 2*r
+dc = (l**2 + a**2 + p**2)**0.5 #diagonal da caixa
+de = 2*r #diametro da esfera
 if dc<=de:
     print("S")
 else:
