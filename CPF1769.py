732d25819a06ff5703bf7a6de3098784d9d01668
Victória Rodrigues Veloso <128431650+victoriavllso@users.noreply.github.com>
2023-04-24 14:40:51 -0300

    Create CPF1769.py

---
+++ CPF1769.py
@@ -0,0 +1,28 @@
+while True:
+    try:
+        CPF = input().strip()
+        cpf = CPF.replace('.', '').replace('-', '') #substitui o ponto e traço por um espaço vazio
+        soma1 = 0
+        soma2 = 0
+        for i in range(9): #|para achar b1
+            soma1 = soma1 + int(cpf[i]) * (i+1) #multiplicamos o primeiro por um, o segundo por dois e assim segue
+        b1 = soma1%11
+        if b1 == 10:
+             b1 = 0
+        for i in range(9): #parar achar b2
+            soma2 = soma2 + int(cpf[i]) * (9-i)
+        b2 = soma2%11
+        if b2 == 10:
+            b2 = 0
+        if b1 == int(CPF[-2]):
+            if b2 == int(CPF[-1]):
+                print("CPF valido")
+            else:
+                print("CPF invalido")
+    
+        else:
+            print("CPF invalido")
+            
+    except EOFError:
+        break
+1769
