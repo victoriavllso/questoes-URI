df1eb2cfd55c95acac16f3da3a0485c20a9ff1c2
Victória Rodrigues Veloso <128431650+victoriavllso@users.noreply.github.com>
2023-05-19 10:33:16 -0300

    Create  2293 - Campo de Minhocas.py

---
+++ 2293 - Campo de Minhocas.py
@@ -0,0 +1,23 @@
+linha, coluna = map(int, input().split())
+mat = [0]*linha
+maiorlinha = 0
+maiorcoluna = 0
+
+for i in range(linha):
+    mat[i] = input().split()
+    for j in range(coluna):
+        mat[i][j] = int(mat[i][j])
+        
+for i in range(linha):
+    somalinha = sum(mat[i]) #faço a soma da linha
+    if somalinha>maiorlinha: #se a soma for maior do que a soma atual, faço a atual se tornar a maior
+        maiorlinha = somalinha
+        
+for j in range(coluna):
+    somacoluna = sum(mat[i][j] for i in range(linha))
+    if somacoluna>maiorcoluna:
+        maiorcoluna = somacoluna
+if maiorlinha > maiorcoluna:
+    print(maiorlinha)
+else:
+    print(maiorcoluna)
